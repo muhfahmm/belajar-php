23efac76580d446db1141704e65f4e6e71fe14ca
muhfahmm <148422221+muhfahmm@users.noreply.github.com>
2025-09-17 19:57:13 +0700

    aa

--- part 2 (crud)/crud 2/index.php
+++ part 2 (crud)/crud 2/index.php
@@ -6,6 +6,29 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <thead>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Kelas</th>
+                <th>Nomor Absen</th>
+                <th>Aksi</th>
+            </thead>
+        </tr>
+        <tr>
+            <tbody>
+                <td>1</td>
+                <td>Fahiim</td>
+                <td>XI-2</td>
+                <td>7</td>
+                <td>
+                    <a href="">hapus</a>
+                    |
+                    <a href="">edit</a>
+                </td>
+            </tbody>
+        </tr>
+    </table>
 </body>
 </html>
